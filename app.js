cfb6aac12a8f464a8f93f25f56def1592648e69a
Daniel Aliu <danielaliu202@gmail.com>
2022-11-17 20:44:35 +0000

    Character counter

---
+++ app.js
@@ -0,0 +1,15 @@
+let fname = document.getElementById("name")
+let email = document.getElementById("email")
+let comment = document.getElementById("comment")
+
+function countChars(obj) {
+    document.getElementById("counter").innerHTML = obj.length +'/140';
+}
+
+
+const commentsList = {
+    firstName: "Jack",
+    emailAdd: "Jackdoe123@gmail.com",
+    comment1: 'Hello Im Jack'
+};
+
